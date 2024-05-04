4d02700349699129a4087c6bee9e4a6b189cb6c2
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-05-03 19:18:38 -0500

    Add permissions screen

---
+++ app/src/main/java/co/edu/udea/compumovil/gr09_20241/android_ml_dojo/ui/PermissionActivity.kt
@@ -0,0 +1,44 @@
+package co.edu.udea.compumovil.gr09_20241.android_ml_dojo.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PermissionScreen(
+    onRequestPermission: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+        ) {
+            Text(
+                textAlign = TextAlign.Center,
+                text = "Please grant the permission to use the camera to use the core functionality of this app."
+            )
+            Button(onClick = onRequestPermission) {
+                Icon(imageVector = Icons.Default.Settings, contentDescription = "Camera")
+                Text(text = "Grant permission")
+            }
+        }
+    }
+}
